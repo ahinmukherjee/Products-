e26aa0c1a704e9248193d7a4ef8db7a946f9c9bb
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-01-25 19:22:46 +0530

    changes

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "../style/header.css";
 
 const Header = () => {
+    const activeLocation = useLocation().pathname;
   return (
-    <header>
-       <Link to="/"> Home </Link>
-       <Link to="/products"> Products</Link>
+    <header className="navber">
+
+      <div className="menuContainer">
+                        <Link class="links"
+                            to="/"
+                            className={activeLocation === "/" ? "menuActive" : ""}
+                        >
+                            Home
+                        </Link>
+
+                        <Link class="links"
+                            to="/products"
+                            className={activeLocation === "/products" ? "menuActive" : ""}
+                        >
+                            Products
+                        </Link>
+        </div>
     </header>
   )
 }
