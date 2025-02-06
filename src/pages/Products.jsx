b1901b02e7933732e6bc183f63a595687a7ed91a
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-02-06 12:09:27 +0530

    final

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -44,40 +44,6 @@ const Products = () => {
 
     // }
     return (
-        // <div>
-        //     <br />
-
-        //     {
-        //         products.map((p) => {
-        //             return <div key={p.id}>
-        //                 <p>Title : {p.title}</p>
-        //                 <img src={p.thumbnail} alt="" />
-        //                 <p>Description : {p.description}</p>
-        //                 <p>Price : ${p.price}</p>
-        //                 <p>Ratting : {p.rating}</p>
-        //                 <p>Category : {p.category}</p>                      
-        //                 <p>Discount offer : {p.discountPercentage}</p>
-        //                 <p>Reviews:
-        //                 { p.reviews && p.reviews.length >0 ? (
-        //                     <ol> 
-        //                         {p.reviews.map((review, index)=>(
-        //                             <li key={index}>
-        //                                 <strong>{review.reviwerName}</strong>{review.comment}
-        //                             </li>
-        //                         ))}
-        //                     </ol>
-        //                 ): ( 
-        //                     <p>No reviews available</p>
-        //                 )}
-        //                 </p>
-
-
-        //             <br />
-        //             </div>
-        //         }
-        //     )}
-        // </div>  
-
     <div className="product-container">
         {products.map((p) => (
             <div className="product-card" key={p.id}>
